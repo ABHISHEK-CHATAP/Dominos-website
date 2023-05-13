eb63ffd6a468c6a17881f92a518fbe32965e1290
ABHISHEK-CHATAP <ABHISHEK-CHATAP@users.noreply.github.com>
2023-05-13 23:36:23 +0000

    deploy: 66822d1979a47050bb7dc3722eadad4c70500ab4

---
+++ Screens/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Home.css"
+import Header from '../Compontent/Header'
+import Body from '../Compontent/Body'
+import Footer from '../Compontent/Footer'
+
+const Home = () => {
+  return (
+   <>
+      <Header />
+      <Body />
+      <Footer />
+   
+      </>
+
+  )
+}
+
+export default Home
